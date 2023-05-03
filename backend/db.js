7358fc26af76c7c8de59fbe1c334a94d4731c35f
Muhammad Daniyal <mr.dannii98@gmail.com>
2023-05-03 17:19:52 +0500

    mongoconnection

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose =require('mongoose');
+
+const mongoURL="mongodb://127.0.0.1:27017";
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURL);
+    //console.log("conntected");
+}
+
+module.exports=connectToMongo;
